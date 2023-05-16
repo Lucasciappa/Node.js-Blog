8e30739f8df91ffd2284ad9b6f263325a53adb0c
Lucas Ciapparelli <lucasciappa97@gmail.com>
2023-05-16 17:28:16 -0300

    add: ejs files && HTML content

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,17 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.send("Hello world")
+
+const locals = {
+    title: 'NodeJS Blog',
+    description: "Simple Blog created with NodeJS, Express & MongoDB."
+}
+
+    res.render('index', {locals});
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 });
 
 module.exports = router;
